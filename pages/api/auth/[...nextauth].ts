85fc3986b826fb220e7cd2320f177496b50c69d3
Mowalky <mowalky@outlook.com>
2022-06-28 22:31:22 -0500

    updated

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,72 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import { comparePassword } from "../../../lib/auth";
+
+export default NextAuth({
+  secret: "sdfasdfasfa",
+  jwt: {
+    secret: "sdfasdfasfa",
+  },
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const res = await fetch("/your/endpoint", {
+          method: "POST",
+          body: JSON.stringify(credentials),
+          headers: { "Content-Type": "application/json" },
+        });
+        const user = await res.json();
+
+        // If no error and we have user data, return it
+        if (res.ok && user) {
+          return user;
+        }
+        // Return null if user data could not be retrieved
+        return null;
+      },
+    }),
+  ],
+});
+
+// export default NextAuth({
+//   session: {
+//     jwt: true,
+//   },
+//   providers: [
+//     CredentialsProvider({
+//       async authorize(credentials) {
+//         const client = await connectToDatabase();
+
+//         const usersCollection = client.db().collection("users");
+//         const user = await usersCollection.findOne({
+//           email: credentials.email,
+//         });
+
+//         if (!user) {
+//           client.close();
+//           throw new Error("No user found!");
+//         }
+
+//         const isValid = await comparePassword(
+//           credentials.password,
+//           user.password
+//         );
+
+//         if (!isValid) {
+//           client.close();
+//           throw new Error("Invalid password!");
+//         }
+
+//         client.close();
+
+//         return { email: user.email };
+//       },
+//     }),
+//   ],
+// });
---
+++ pages/auth.tsx
@@ -0,0 +1,51 @@
+import { useState, useRef } from "react";
+import { useRouter } from "next/router";
+import { signIn } from "next-auth/react";
+
+function AuthForm() {
+  const emailInputRef = useRef<HTMLInputElement | null>(null);
+  const passwordInputRef = useRef<HTMLInputElement | null>(null);
+  const router = useRouter();
+
+  async function submitHandler(
+    event: React.FormEvent<HTMLFormElement>
+  ): Promise<void> {
+    event.preventDefault();
+
+    const result = await signIn("credentials", {
+      redirect: false,
+      email: emailInputRef?.current?.value,
+      password: passwordInputRef?.current?.value,
+    });
+    console.log(result);
+
+    if (!result?.error) {
+      router.replace("/profile");
+    }
+  }
+
+  return (
+    <section>
+      <form onSubmit={submitHandler}>
+        <div>
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" ref={emailInputRef} required />
+        </div>
+        <div>
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            ref={passwordInputRef}
+            required
+          />
+        </div>
+        <div>
+          <button>Login</button>
+        </div>
+      </form>
+    </section>
+  );
+}
+
+export default AuthForm;
